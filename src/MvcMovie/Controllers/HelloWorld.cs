509265bf068cc8b66f5f383903b4a1d26e2fb851
Gregory Adler <greg7239@gmail.com>
2017-02-12 15:54:56 -0500

    Restoring Files Changed Homepage

---
+++ src/MvcMovie/Controllers/HelloWorld.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
---
+++ src/MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
